b0c99b2c31918e8c77f2bca13f76e986224f3064
Mathieu <ackermann.mathieu@gmail.com>
2025-11-11 18:51:03 +0100

    feat: add api with users and pollutions

---
+++ api/models/users.model.ts
@@ -0,0 +1,24 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+export const usersModel = (sequelize: Sequelize) => {
+  const Users = sequelize.define("users", {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+    },
+    login: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  });
+  return Users;
+};
