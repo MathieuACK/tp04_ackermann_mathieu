766fdcf5c4a0a023a0ea57af8f17271ba80e1d9e
Mathieu <ackermann.mathieu@gmail.com>
2025-11-11 20:01:50 +0100

    feat: add ihm for login & signup

--- Front/src/app/components/login/login.component.ts
+++ Front/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { UsersService } from '../../services/users/users.service';
 import { AuthService } from '../../services/auth/auth.service';
@@ -7,14 +8,17 @@ import { AuthService } from '../../services/auth/auth.service';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  firstname = '';
+  lastname = '';
   login = '';
   password = '';
   error = '';
+  signupMode = false;
 
   constructor(
     private auth: AuthService,
@@ -23,12 +27,34 @@ export class LoginComponent {
   ) {}
 
   submit() {
-    this.auth.login(this.login, this.password).subscribe((ok) => {
-      if (ok) {
-        this.router.navigate(['/pollution/list']);
-      } else {
-        this.error = 'Identifiants invalides';
-      }
-    });
+    if (!this.signupMode) {
+      this.auth.login(this.login, this.password).subscribe((ok) => {
+        if (ok) {
+          this.router.navigate(['/pollution/list']);
+        } else {
+          this.error = 'Identifiants invalides';
+        }
+      });
+    } else {
+      // attempt signup
+      const user = {
+        login: this.login,
+        password: this.password,
+        firstname: this.firstname,
+        lastname: this.lastname,
+      };
+      this.auth.signup(user).subscribe((created) => {
+        if (created) {
+          this.router.navigate(['/pollution/list']);
+        } else {
+          this.error = 'Impossible de créer le compte (login peut exister)';
+        }
+      });
+    }
+  }
+
+  toggleSignup() {
+    this.signupMode = !this.signupMode;
+    this.error = '';
   }
 }
--- Front/src/app/services/auth/auth.service.ts
+++ Front/src/app/services/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, catchError, of } from 'rxjs';
 import { UsersService } from '../users/users.service';
 import { User } from '../../models/users';
 
@@ -25,6 +25,23 @@ export class AuthService {
           return true;
         }
         return false;
+      })
+    );
+  }
+
+  public signup(user: Partial<User>): Observable<boolean> {
+    return this.usersService.addUser(user).pipe(
+      map((created) => {
+        if (created) {
+          this.currentUser$.next(created as User);
+          localStorage.setItem('currentUser', JSON.stringify(created));
+          return true;
+        }
+        return false;
+      }),
+      catchError((err) => {
+        // return false on error (e.g., 409 conflict)
+        return of(false);
       })
     );
   }
